759c40378d758200cea4bcf45d1bd21758290508
Preetam Varun <preetamarun99@gmail.com>
2022-11-24 18:59:49 +0000

    will continue coding after 30-45mins

--- src/Dashboardcomponent/Dashboardcomponent.js
+++ src/Dashboardcomponent/Dashboardcomponent.js
@@ -5,7 +5,7 @@ class Dashboardcomponent extends Component{
     render(){
         return(
             <div className = "Main-Dashboard">
-                <h1>How do we do ?</h1>
+                <h1>How did we do?</h1>
                 <p>Please let us know how did we do with your support request. All feedback is appreciated to
                 help us improve our offering!</p>
                 <div className="Rating-Links">
@@ -15,7 +15,7 @@ class Dashboardcomponent extends Component{
                     <span className="Rating-Value">4</span>
                     <span className="Rating-Value">5</span>
                 </div>
-                <a href = "/#" className="Submit-Btn">Submit</a>
+                <a href = "/#" className="Submit-Btn">SUBMIT</a>
             </div>
         )
     }
