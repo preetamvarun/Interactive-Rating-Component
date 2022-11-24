49d134cd4d9f47833261734999d8e3e2231b9b25
Preetam Varun <preetamarun99@gmail.com>
2022-11-24 17:38:00 +0000

    started coding dashboard component. styling things in dashboard component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import Dashboardcomponent from "./Dashboardcomponent/Dashboardcomponent";
 
 function App() {
   return (
     <div className="App">
-      
+      <Dashboardcomponent/>
     </div>
   );
 }
---
+++ src/Dashboardcomponent/Dashboardcomponent.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+import './Dashboardstyles.css';
+
+class Dashboardcomponent extends Component{
+    render(){
+        return(
+            <div className = "Main-Dashboard">
+                <h1>How do we do ?</h1>
+                <p>Please let us know how did we do with your support request. All feedback is appreciated to
+                help us improve our offering!</p>
+                <div className="Rating-Links">
+                    <span className="Rating-Value">1</span>
+                    <span className="Rating-Value">2</span>
+                    <span className="Rating-Value">3</span>
+                    <span className="Rating-Value">4</span>
+                    <span className="Rating-Value">5</span>
+                </div>
+                <a href = "/#" className="Submit-Btn">Submit</a>
+            </div>
+        )
+    }
+}
+
+export default Dashboardcomponent;
